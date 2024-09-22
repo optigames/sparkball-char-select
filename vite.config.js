0e66b2554f7989de25727e33c37fa525784a39de
Austin <austin.harvey@beopti.gg>
2024-09-22 19:20:33 -0400

    Update favicon, title, and default path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sparkball-char-select/dist/",
+  // base: "/sparkball-char-select/dist/",
 });
